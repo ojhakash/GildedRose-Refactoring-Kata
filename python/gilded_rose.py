3d4ba859e7be48eb8cd188ee5589238e1a83d591
__akash_ojha__ <akashojha15@gmail.com>
2025-03-22 16:13:29 +0530

    implement Factory design pattern for different item creation

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -15,35 +15,69 @@ class GildedRose(object):
         for item in self.items:
             item.update_quality()
 
+class ItemFactory:
+    @staticmethod
+    def create_item(item):
+        if item.name == ITEM_NAMES['AGED_BRIE']:
+            return AgedBrie(item)
+        elif item.name == ITEM_NAMES['BACKSTAGE_PASSES']:
+            return BackstagePasses(item)
+        elif item.name == ITEM_NAMES['SULFURAS']:
+            return Sulfuras(item)
+        else:
+            return RegularItem(item)
+
 class ShopItem:
+    def __init__(self, item):
+        self.item = item
+        self.item_type = ItemFactory.create_item(item)
+
+    def update_quality(self):
+        self.item_type.update_quality()
+
+class AgedBrie:
     def __init__(self, item):
         self.item = item
 
     def update_quality(self):
-        if self.item.name != ITEM_NAMES['BACKSTAGE_PASSES'] and self.item.name != ITEM_NAMES['AGED_BRIE']:
-            if self.item.name != ITEM_NAMES['SULFURAS'] and self.item.quality > 0:
-                self.item.quality = self.item.quality - 1
-        else:
-            if self.item.quality < 50:
-                self.item.quality = self.item.quality + 1
-                if self.item.name == ITEM_NAMES['BACKSTAGE_PASSES']:
-                    if self.item.sell_in < 11 and self.item.quality < 50:
-                        self.item.quality = self.item.quality + 1
-                    if self.item.sell_in < 6 and self.item.quality < 50:
-                        self.item.quality = self.item.quality + 1
-        if self.item.name != ITEM_NAMES['SULFURAS']:
-            self.item.sell_in = self.item.sell_in - 1
-        if self.item.sell_in < 0:
-            if self.item.name != ITEM_NAMES['AGED_BRIE']:
-                if self.item.name != ITEM_NAMES['BACKSTAGE_PASSES']:
-                    if self.item.name != ITEM_NAMES['SULFURAS'] and self.item.quality > 0:
-                        self.item.quality = self.item.quality - 1
-                else:
-                    self.item.quality = 0
-            else:
-                if self.item.quality < 50:
-                    self.item.quality = self.item.quality + 1
+        if self.item.quality < 50:
+            self.item.quality += 1
+        self.item.sell_in -= 1
+        if self.item.sell_in < 0 and self.item.quality < 50:
+            self.item.quality += 1
 
+class BackstagePasses:
+    def __init__(self, item):
+        self.item = item
+
+    def update_quality(self):
+        if self.item.quality < 50:
+            self.item.quality += 1
+            if self.item.sell_in < 11 and self.item.quality < 50:
+                self.item.quality += 1
+            if self.item.sell_in < 6 and self.item.quality < 50:
+                self.item.quality += 1
+        self.item.sell_in -= 1
+        if self.item.sell_in < 0:
+            self.item.quality = 0
+
+class Sulfuras:
+    def __init__(self, item):
+        self.item = item
+
+    def update_quality(self):
+        pass
+
+class RegularItem:
+    def __init__(self, item):
+        self.item = item
+
+    def update_quality(self):
+        if self.item.quality > 0:
+            self.item.quality -= 1
+        self.item.sell_in -= 1
+        if self.item.sell_in < 0 and self.item.quality > 0:
+            self.item.quality -= 1
 
 class Item:
     def __init__(self, name, sell_in, quality):
