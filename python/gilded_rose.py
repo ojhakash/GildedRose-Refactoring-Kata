c6c33378988645d2d8401f5acc2cc241fae4603d
__akash_ojha__ <akashojha15@gmail.com>
2025-03-22 15:15:25 +0530

    replace string constants with ITEM_NAMES dictionary for better maintainability

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
-# Item name constants
-AGED_BRIE = "Aged Brie"
-BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
-SULFURAS = "Sulfuras, Hand of Ragnaros"
+ITEM_NAMES = {
+    'AGED_BRIE': "Aged Brie",
+    'BACKSTAGE_PASSES': "Backstage passes to a TAFKAL80ETC concert",
+    'SULFURAS': "Sulfuras, Hand of Ragnaros"
+}
 
 class GildedRose(object):
 
@@ -19,24 +20,24 @@ class ShopItem:
         self.item = item
 
     def update_quality(self):
-        if self.item.name != BACKSTAGE_PASSES and self.item.name != AGED_BRIE:
-            if self.item.name != SULFURAS and self.item.quality > 0:
+        if self.item.name != ITEM_NAMES['BACKSTAGE_PASSES'] and self.item.name != ITEM_NAMES['AGED_BRIE']:
+            if self.item.name != ITEM_NAMES['SULFURAS'] and self.item.quality > 0:
                 self.item.quality = self.item.quality - 1
         else:
             if self.item.quality < 50:
                 self.item.quality = self.item.quality + 1
-                if self.item.name == BACKSTAGE_PASSES:
+                if self.item.name == ITEM_NAMES['BACKSTAGE_PASSES']:
                     if self.item.sell_in < 11 and self.item.quality < 50:
-                            self.item.quality = self.item.quality + 1
+                        self.item.quality = self.item.quality + 1
                     if self.item.sell_in < 6 and self.item.quality < 50:
-                            self.item.quality = self.item.quality + 1
-        if self.item.name != SULFURAS:
+                        self.item.quality = self.item.quality + 1
+        if self.item.name != ITEM_NAMES['SULFURAS']:
             self.item.sell_in = self.item.sell_in - 1
         if self.item.sell_in < 0:
-            if self.item.name != AGED_BRIE:
-                if self.item.name != BACKSTAGE_PASSES:
-                    if self.item.name != SULFURAS and self.item.quality > 0:
-                            self.item.quality = self.item.quality - 1
+            if self.item.name != ITEM_NAMES['AGED_BRIE']:
+                if self.item.name != ITEM_NAMES['BACKSTAGE_PASSES']:
+                    if self.item.name != ITEM_NAMES['SULFURAS'] and self.item.quality > 0:
+                        self.item.quality = self.item.quality - 1
                 else:
                     self.item.quality = 0
             else:
