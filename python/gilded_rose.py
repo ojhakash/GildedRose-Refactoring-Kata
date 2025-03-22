cbf8d9b1d7536760f27f15baf1288faafe8e5f28
__akash_ojha__ <akashojha15@gmail.com>
2025-03-22 19:57:57 +0530

    added Conjured item while testcases failed

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -3,7 +3,8 @@
 ITEM_NAMES = {
     'AGED_BRIE': "Aged Brie",
     'BACKSTAGE_PASSES': "Backstage passes to a TAFKAL80ETC concert",
-    'SULFURAS': "Sulfuras, Hand of Ragnaros"
+    'SULFURAS': "Sulfuras, Hand of Ragnaros",
+    'CONJURED': "Conjured Mana Cake"
 }
 
 class GildedRose(object):
@@ -24,6 +25,8 @@ class ItemFactory:
             return BackstagePasses(item)
         elif item.name == ITEM_NAMES['SULFURAS']:
             return Sulfuras(item)
+        elif item.name == ITEM_NAMES['CONJURED']:
+            return ConjuredItem(item)
         else:
             return RegularItem(item)
 
@@ -78,6 +81,17 @@ class RegularItem:
         self.item.sell_in -= 1
         if self.item.sell_in < 0 and self.item.quality > 0:
             self.item.quality -= 1
+
+class ConjuredItem:
+    def __init__(self, item):
+        self.item = item
+
+    def update_quality(self):
+        if self.item.quality > 0:
+            self.item.quality = max(0, self.item.quality - 2)
+        self.item.sell_in -= 1
+        if self.item.sell_in < 0 and self.item.quality > 0:
+            self.item.quality = max(0, self.item.quality - 2)
 
 class Item:
     def __init__(self, name, sell_in, quality):
